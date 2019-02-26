e900a23f84f626cc82d1b86a75f24bc738512d87
薛凯 <3440395@qq.com>
2019-02-26 08:33:48 +0800

    ..

--- app/src/main/java/com/xk/myapplication/MainActivity.java
+++ app/src/main/java/com/xk/myapplication/MainActivity.java
@@ -1,12 +1,14 @@
 package com.xk.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Application;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Application
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
