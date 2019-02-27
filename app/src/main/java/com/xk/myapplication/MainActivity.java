0e3369f7f48d001f5dcbdf606544ae98ab710b14
薛凯 <3440395@qq.com>
2019-02-27 08:28:02 +0800

    ..

--- app/src/main/java/com/xk/myapplication/MainActivity.java
+++ app/src/main/java/com/xk/myapplication/MainActivity.java
@@ -1,6 +1,5 @@
 package com.xk.myapplication;
 
-import android.app.Application;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -8,7 +7,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Application
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
